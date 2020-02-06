2586bd03a229e456fb19f3f999cb6e8d1eeb2068
o-strekalova <strekalova.olg@yandex.ru>
2020-02-06 17:29:17 +0300

    Добавляет сетки на карту

--- js/main.js
+++ js/main.js
@@ -6,7 +6,10 @@ var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var TIMES = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var numberOfOffers = 8;
+var numberOfPins = 8;
+
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
 
 var getRandomInt = function (min, max) {
   min = Math.ceil(min);
@@ -17,20 +20,20 @@ var getRandomInt = function (min, max) {
 var getOffers = function () {
   var offers = [];
 
-  for (var i = 1; i < numberOfOffers + 1; i++) {
+  for (var i = 0; i < numberOfPins; i++) {
     var offersItem = {};
 
     offersItem.author = {};
-    offersItem.author.avatar = 'img/avatars/user0' + i + '.png';
+    offersItem.author.avatar = 'img/avatars/user0' + (i + 1) + '.png';
 
     offersItem.location = {};
-    offersItem.location.x = getRandomInt(0, 1001);
-    offersItem.location.y = getRandomInt(130, 630);
+    offersItem.location.x = getRandomInt(25, 1176);
+    offersItem.location.y = getRandomInt(130, 631);
 
     offersItem.offer = {};
     offersItem.offer.title = TITLES[getRandomInt(0, TITLES.length)];
     offersItem.offer.address = offersItem.location.x + ', ' + offersItem.location.y;
-    offersItem.offer.price = getRandomInt(1, 101) * 1000;
+    offersItem.offer.price = getRandomInt(1, 11) * 1000;
     offersItem.offer.type = TYPES[getRandomInt(0, TYPES.length)];
     offersItem.offer.rooms = getRandomInt(1, 11);
     offersItem.offer.guests = getRandomInt(1, 101);
@@ -38,15 +41,15 @@ var getOffers = function () {
     offersItem.offer.checkout = TIMES[getRandomInt(0, TIMES.length)];
     offersItem.offer.features = [];
 
-    for (var a = 0; a < getRandomInt(1, FEATURES.length + 1); a++) {
-      offersItem.offer.features[a] = FEATURES[a];
+    for (var j = 0; j < getRandomInt(1, FEATURES.length); j++) {
+      offersItem.offer.features[j] = FEATURES[j];
     }
 
     offersItem.offer.description = DESCRIPTIONS[getRandomInt(0, DESCRIPTIONS.length)];
     offersItem.offer.photos = [];
 
-    for (var b = 0; b < getRandomInt(1, PHOTOS.length + 1); b++) {
-      offersItem.offer.photos[b] = PHOTOS[b];
+    for (var k = 0; k < getRandomInt(1, PHOTOS.length); k++) {
+      offersItem.offer.photos[k] = PHOTOS[k];
     }
 
     offers[i] = offersItem;
@@ -55,30 +58,28 @@ var getOffers = function () {
   return offers;
 };
 
-var offers = getOffers(8);
-console.log(offers);
-
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+var offers = getOffers(numberOfPins);
 
 var pinsList = document.querySelector('.map__pins');
-var pinTemplate = document.querySelector('#pin');
+var pinTemplate = document.querySelector('#pin')
+    .content
+    .querySelector('.map__pin');
 
-var renderPin = function (offer) {
+var renderPin = function (offerItem) {
   var pinElement = pinTemplate.cloneNode(true);
 
-  pinElement.style.left = offer.location.x - 78 + 'px';
-  pinElement.style.top = offer.location.y - 156 + 'px';
+  pinElement.style.left = offerItem.location.x - 25 + 'px';
+  pinElement.style.top = offerItem.location.y - 70 + 'px';
 
-  var pinAvatar = pinTemplate.content.querySelector('img');
-  pinAvatar.src = offer.author.avatar;
-  pinAvatar.alt = offer.title;
+  var pinAvatar = pinTemplate.querySelector('img');
+  pinAvatar.src = offerItem.author.avatar;
+  pinAvatar.alt = offerItem.offer.title;
 
   return pinElement;
 };
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < offers.length; i++) {
-  fragment.appendChild(renderPin(offers[i]));
+for (var m = 0; m < offers.length; m++) {
+  fragment.appendChild(renderPin(offers[m]));
 }
 pinsList.appendChild(fragment);

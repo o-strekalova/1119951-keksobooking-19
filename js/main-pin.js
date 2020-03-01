c3234b74efa12d9ea505e6e164291ce858bc63fc
o-strekalova <strekalova.olg@yandex.ru>
2020-03-02 00:38:27 +0300

    Перетаскивание метки

---
+++ js/main-pin.js
@@ -0,0 +1,52 @@
+'use strict';
+
+(function () {
+  window.pinMain.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      window.pinMain.style.top = (window.pinMain.offsetTop - shift.y) + 'px';
+      window.pinMain.style.left = (window.pinMain.offsetLeft - shift.x) + 'px';
+
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          window.pinMain.removeEventListener('click', onClickPreventDefault);
+        };
+        window.pinMain.addEventListener('click', onClickPreventDefault);
+      }
+
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+})();

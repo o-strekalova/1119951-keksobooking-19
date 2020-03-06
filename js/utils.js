ab16297bd888283a72ce07ce680b7d1e7d93b7b2
o-strekalova <strekalova.olg@yandex.ru>
2020-03-06 18:18:12 +0300

    Начало отправки формы на сервер

--- js/utils.js
+++ js/utils.js
@@ -9,4 +9,16 @@
   window.form = document.querySelector('.ad-form');
   window.addressInput = document.getElementById('address');
   window.formElements = document.querySelectorAll('.ad-form__element');
+
+  window.onError = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
 })();
